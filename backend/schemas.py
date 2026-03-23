36c6e70ceb834794c28b02fbe188e1ea6b6d6862
akshayjayesh <akshayjayeshjp@gmail.com>
2026-03-23 12:14:19 +0530

    fullstack app ready

---
+++ backend/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title: str
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+
+    class Config:
+        from_attributes = True   # ✅ updated
